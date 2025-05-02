2333383c0f7fe2315505b525793c46951ea5ce54
DanielLoo03 <dloog23@gmail.com>
2025-05-01 18:22:43 -0700

    Se modifica ProductList.tsx

--- components/ui/ProductList.tsx
+++ components/ui/ProductList.tsx
@@ -4,13 +4,23 @@ import { useState } from "react";
 import ProductCard from "@/components/ui/ProductCard";
 
 export default function ProductList({ products }: { products: any[] }) {
-  const [sortedProducts, setSortedProducts] = useState(products);
+  const [sortedProducts, setSortedProducts] = useState(
+    [...products].sort((a, b) => a.name.localeCompare(b.name))
+  );
 
   const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const sortOption = e.target.value;
     let sorted = [...products];
 
-    // AQUI TU CODIGO DE ORDENAMIENTO
+    if (sortOption === "price-asc") {
+      sorted.sort((a, b) => a.price - b.price);
+    } else if (sortOption === "price-desc") {
+      sorted.sort((a, b) => b.price - a.price);
+    } else if (sortOption === "alphabetical") {
+      sorted.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortOption === "less30") {
+      sorted = sorted.filter((product) => product.price < 30);
+    }
 
     setSortedProducts(sorted);
   };
@@ -25,6 +35,7 @@ export default function ProductList({ products }: { products: any[] }) {
           id="sort"
           className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           onChange={handleSort}
+          defaultValue="alphabetical"
         >
           <option value="price-asc">Precio: Menor a Mayor</option>
           <option value="price-desc">Precio: Mayor a Menor</option>
